d5bb7d8854d8ce5b066942ae32108c2db0e8084a
SESA570968 <SESA570968@WTIN05204099L.apa.gad.schneider-electric.com>
2020-05-12 00:46:35 +0530

    wefsdf

--- JavaAntProject1/src/com/package1/testclass.java
+++ JavaAntProject1/src/com/package1/testclass.java
@@ -3,7 +3,8 @@ package com.package1;
 public class testclass {
 	public static void main(String args[])
 	{
-		System.out.println("test class");
+		System.out.println("changes done here ");
+		System.out.println("changes done here too");
 	}
 
 }
